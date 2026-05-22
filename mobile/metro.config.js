856caad6c5c5876db74f3da2777b849851ee20d2
How0515 <bingos6258@gmail.com>
2026-05-23 01:32:56 +0900

    fix(mobile): Metro 설정 추가로 지갑 연동 번들링 안정화
    - 모바일 앱용 metro.config.js 추가
    - React Native 조건 해석 우선순위 지정
    - WalletConnect/MetaMask 계열 lazy import 대응
    - inlineRequires 비활성화로 unknown module 숫자 id 에러 완화

---
+++ mobile/metro.config.js
@@ -0,0 +1,19 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_conditionNames = [
+  'react-native',
+  'import',
+  'require',
+  'default',
+];
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
+  },
+});
+
+module.exports = config;
